4912f41e501b128b16a59a753b659a15b122e4ca
ArisUwU <77840580+Cris1Z@users.noreply.github.com>
2026-03-19 21:54:26 +0800

    Add condition to stop input processing during scroll

--- src/touch_detection.c
+++ src/touch_detection.c
@@ -44,6 +44,9 @@ int touch_detection_handle_event(const struct device *dev, struct input_event *e
         // is this true? 
         // because sometimes we might not want to forward half the events!
         // for example when waiting during a tap or while a scroll is happening
+        if (data->circular_scroll.is_tracking) {
+            return ZMK_INPUT_PROC_STOP;
+        }
         return ZMK_INPUT_PROC_CONTINUE;
     }
 
